9673ef9f898bb4c55da1c102c1140fdb344c8cd6
Ihimbru-k <kanihims25@gmail.com>
2023-09-05 13:25:11 +0100

    working on root screen

---
+++ lib/widgets/root_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+
+
+class RootScreen extends StatefulWidget {
+  const RootScreen({Key? key}) : super(key: key);
+
+  @override
+  State<RootScreen> createState() => _RootScreenState();
+}
+
+class _RootScreenState extends State<RootScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(children: [
+
+      ],),
+    );
+  }
+}
