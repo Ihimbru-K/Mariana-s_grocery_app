e2d7ac8c866ece32c2f5a137a32a4f8c91ea9cb1
Ihimbru-k <kanihims25@gmail.com>
2023-09-05 17:02:58 +0100

    added some new directories

---
+++ lib/inner_screens/deals_and_shopping/deals/deals.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Deals extends StatefulWidget {
+  const Deals({Key? key}) : super(key: key);
+
+  @override
+  State<Deals> createState() => _DealsState();
+}
+
+class _DealsState extends State<Deals> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
