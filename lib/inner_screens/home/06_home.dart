4111bfdc02d5ffdc6a961413570dde4f7b016fff
Ihimbru-k <kanihims25@gmail.com>
2023-09-05 13:59:10 +0100

    working on appbar of home page

--- lib/inner_screens/home/06_home.dart
+++ lib/inner_screens/home/06_home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../constants/colors.dart';
+
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
@@ -7,9 +9,27 @@ class Home extends StatefulWidget {
   State<Home> createState() => _HomeState();
 }
 
+
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    const String name = "Rafatul Islam";
+    return Scaffold(
+      appBar: AppBar(
+
+        actions: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [ Column(children: const [
+            Text("Good morning", style: TextStyle(fontSize: 13, color: dgrey),), SizedBox(height: 3,), Text("$name", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+          ],),
+              Icon(Icons.notification_add_rounded)
+              
+        ],)],
+      ),
+      body: Container(child: Column(
+        children: [Text("This is ")],
+      ),),
+    );
   }
 }
