050095e73210c0d5ace8158c235fa102747b4aee
Ihimbru-k <kanihims25@gmail.com>
2023-09-06 11:52:04 +0100

    working on deals module

--- lib/inner_screens/deals_and_shopping/deals/item_deals.dart
+++ lib/inner_screens/deals_and_shopping/deals/item_deals.dart
@@ -10,6 +10,9 @@ class ItemsDeals extends StatefulWidget {
 class _ItemsDealsState extends State<ItemsDeals> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return  Scaffold(
+      appBar: AppBar(),
+      body: Container(),
+    );
   }
 }
