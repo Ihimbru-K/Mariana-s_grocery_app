5a9b8e846e9cd000f9aa7e2b4bf3f48ddb1d61cf
Ihimbru-k <kanihims25@gmail.com>
2023-09-09 14:19:01 +0100

    working on review rating module

--- lib/inner_screens/deals_and_shopping/shopping/write_reviews.dart
+++ lib/inner_screens/deals_and_shopping/shopping/write_reviews.dart
@@ -13,6 +13,7 @@ class WriteReviews extends StatefulWidget {
 class _WriteReviewsState extends State<WriteReviews> {
   @override
   Widget build(BuildContext context) {
+    final double screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: AppBar(leading: const CircleAvatar(
         //radius: screenWidth * 0.024,
@@ -27,7 +28,18 @@ class _WriteReviewsState extends State<WriteReviews> {
 
       ),
 
-      body: ,
+      body: Container(
+        child: Column(children:  [
+
+            const Center(child: Text("Tell Us to Improve", style: TextStyle(color: black, fontSize: 28, fontWeight: FontWeight.bold),),),
+
+          SizedBox(height: screenHeight*0.03,),
+          const Center(child: Text("Lorem ipsum dolor sit amet, consectetur", style: TextStyle(color: black,fontSize: 16),),),
+          const Center(child: Text("adipiscing elit, sed do eiusmod tempor ", style: TextStyle(color: black,fontSize: 16),),),
+          gi
+
+        ],),
+      ),
     );
   }
 }
