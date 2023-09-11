76c252bf5fbc08acca9a4d8ac80bda0962293c02
Ihimbru-k <kanihims25@gmail.com>
2023-09-11 06:38:05 +0100

    added a slider

--- lib/inner_screens/deals_and_shopping/shopping/order_tracking2.dart
+++ lib/inner_screens/deals_and_shopping/shopping/order_tracking2.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-
 import '../../../constants/colors.dart';
-
 
 class OrderTracking extends StatefulWidget {
   const OrderTracking({Key? key}) : super(key: key);
@@ -11,6 +9,10 @@ class OrderTracking extends StatefulWidget {
 }
 
 class _OrderTrackingState extends State<OrderTracking> {
+  double _value1 = 0.0;
+  double _value2 = 0.5;
+  double _value3 = 1.0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,19 +21,79 @@ class _OrderTrackingState extends State<OrderTracking> {
           backgroundColor: Colors.orange,
           child: Icon(
             Icons.close,
-            color: black,
+            color: Colors.black,
           ),
         ),
-        title: const Text("Order Tracking", style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),),
+        title: const Text(
+          "Order Tracking",
+          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+        ),
         centerTitle: true,
-
       ),
+      body: Container(
+        child: Column(
+          children: [
+            Image.asset("assets/images/bike.png"),
+            SliderTheme(
+              data: SliderThemeData(
+                thumbColor: Colors.orange,
+                activeTrackColor: Colors.orange,
+                inactiveTrackColor: Colors.grey,
+                overlayColor: Colors.orange.withOpacity(0.2),
+              ),
+              child: Column(
+                children: [
+                  _buildSliderRow('Step 1', 'Lorem ipsum dolor sit amet, adipiscing elit, sed do eiusmod', _value1, 1),
+                  _buildSliderRow('Step 2', 'Lorem ipsum dolor sit amet, adipiscing elit, sed do eiusmod', _value2, 2),
+                  _buildSliderRow('Step 3', 'Lorem ipsum dolor sit amet, adipiscing elit, sed do eiusmod', _value3, 3),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 
-      body: Container(child: Column(children: [
-
-
-
-      ],),),
+  Widget _buildSliderRow(String title, String description, double value, int nodeIndex) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Expanded(
+          flex: 2,
+          child: Slider(
+            value: value,
+            onChanged: (newValue) {
+              // No need for setState since the value is not being used to update the state
+            },
+            min: 0.0,
+            max: 1.0,
+            divisions: 10,
+            label: title,
+            vertical: true,
+          ),
+        ),
+        Expanded(
+          flex: 3,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Node $nodeIndex - $title',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                description,
+                style: TextStyle(
+                  color: Colors.grey,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
