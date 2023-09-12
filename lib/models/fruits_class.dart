8628395e51fc538f137a2526177065422521e0a3
Ihimbru-k <kanihims25@gmail.com>
2023-09-12 13:07:28 +0100

    refactoring

--- lib/models/fruits_class.dart
+++ lib/models/fruits_class.dart
@@ -61,9 +61,18 @@ class FruitSelectionScreen extends StatefulWidget {
 class _FruitSelectionScreenState extends State<FruitSelectionScreen> {
   final Cart cart = Cart();
   int counter = 0;
+  int selectedHeadingIndex = 0;
+  double price = 4.9;
+  double rating = 4.5;
+  double reviews = 128;
+
+
 
   @override
   Widget build(BuildContext context) {
+    final double screenWidth = MediaQuery.of(context).size.width;
+    final double screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Fruit Selection'),
@@ -91,12 +100,107 @@ class _FruitSelectionScreenState extends State<FruitSelectionScreen> {
             ),
             child: Column(
             children: [
+              Column(
+             // crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [Text("Fruits", style: TextStyle(fontSize: screenWidth*0.04,  fontWeight: FontWeight.bold),), Padding(
+                padding: EdgeInsets.all(screenWidth * 0.04),
+                child: Text(
+                  'Fresh Orange',
+                  style: TextStyle(
+                    fontSize: screenWidth * 0.05,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              ],),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("$price", style: TextStyle(color: lightYellow, fontSize: 25),),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            if (counter > 0) {
+                              counter--;
+                            }
+                          });
+                        },
+                        child: Container(
+                          width: 40,
+                          height: 40,
+                          decoration: BoxDecoration(
+                            color: lightYellow,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: const Center(
+                            child: Text(
+                              "-",
+                              style: TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 16),
+                      Text(
+                        "$counter",
+                        style: TextStyle(fontSize: 24),
+                      ),
+                      SizedBox(width: 16),
+                      GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            counter++;
+                          });
+                        },
+                        child: Container(
+                          width: 40,
+                          height: 40,
+                          decoration: BoxDecoration(
+                            color: lightYellow,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: const Center(
+                            child: Text(
+                              "+",
+                              style: TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],),
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(children: [
+                    const Icon(Icons.star, color: lightYellow,),
+                    Text("$rating", style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 23),),SizedBox(width:4,),
+                    Text("($reviews reviews,)", style: const TextStyle(color: dgrey),)
+                  ], ), Row(children: [Image.asset("assets/images/people.png", height: 100, width: 100,)],)
+                ],),
+
+
+
+
+
               ListTile(
                // leading: Image.asset(
                  // 'assets/images/orange.png',
                   //width: 50,
                   //height: 50,
                // ),
+
+
                 title: Text('Banana'),
                 subtitle: Text('Category: Fruits'),
                 trailing: Text('Price: \$7.00'),
@@ -165,10 +269,10 @@ class _FruitSelectionScreenState extends State<FruitSelectionScreen> {
               ElevatedButton(
                 onPressed: () {
                   final fruit = Fruit(
-                    name: 'Banana',
+                    name: 'Orange',
                     category: 'Fruits',
                     unitPrice: 7.0,
-                    image: 'assets/images/banana.png',
+                    image: 'assets/images/orange.png',
                   );
                   cart.addToCart(fruit);
                   Navigator.push(
