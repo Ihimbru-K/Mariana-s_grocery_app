5f2709e6a3b1c8016403dade7d9a4866a7cca843
Ihimbru-k <kanihims25@gmail.com>
2023-09-12 12:44:21 +0100

    refactoring items into classes

--- lib/models/fruits_class.dart
+++ lib/models/fruits_class.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../constants/colors.dart';
 import 'cart_screen.dart';
 
 class Fruit {
@@ -67,99 +68,122 @@ class _FruitSelectionScreenState extends State<FruitSelectionScreen> {
       appBar: AppBar(
         title: Text('Fruit Selection'),
       ),
-      body: Column(
-        children: [
-          ListTile(
-            leading: Image.asset(
-              'assets/images/banana.png',
-              width: 50,
-              height: 50,
-            ),
-            title: Text('Banana'),
-            subtitle: Text('Category: Fruits'),
-            trailing: Text('Price: \$7.00'),
+      body: Stack(children: [
+        // Background Image
+        Positioned.fill(
+          child: Image.asset(
+            'assets/images/girl.png',
+            fit: BoxFit.cover,
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+        ),
+        Positioned(
+          top: MediaQuery.of(context).size.height * 0.45 -
+              MediaQuery.of(context).size.height * 0.2,
+          left: MediaQuery.of(context).size.width * 0.001,
+          right: MediaQuery.of(context).size.width * 0.001,
+          child: Container(
+
+            decoration: const BoxDecoration(
+              color: white,
+              borderRadius: BorderRadius.vertical(
+                top: Radius.circular(30.0), // Round the top corners
+              ),
+            ),
+            child: Column(
             children: [
-              GestureDetector(
-                onTap: () {
-                  setState(() {
-                    if (counter > 0) {
-                      counter--;
-                    }
-                  });
-                },
-                child: Container(
-                  width: 40,
-                  height: 40,
-                  decoration: BoxDecoration(
-                    color: Colors.orange,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: const Center(
-                    child: Text(
-                      "-",
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
+              ListTile(
+               // leading: Image.asset(
+                 // 'assets/images/orange.png',
+                  //width: 50,
+                  //height: 50,
+               // ),
+                title: Text('Banana'),
+                subtitle: Text('Category: Fruits'),
+                trailing: Text('Price: \$7.00'),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        if (counter > 0) {
+                          counter--;
+                        }
+                      });
+                    },
+                    child: Container(
+                      width: 40,
+                      height: 40,
+                      decoration: BoxDecoration(
+                        color: Colors.orange,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: const Center(
+                        child: Text(
+                          "-",
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ),
-              const SizedBox(width: 16),
-              Text(
-                "$counter",
-                style: TextStyle(fontSize: 24),
-              ),
-              SizedBox(width: 16),
-              GestureDetector(
-                onTap: () {
-                  setState(() {
-                    counter++;
-                  });
-                },
-                child: Container(
-                  width: 40,
-                  height: 40,
-                  decoration: BoxDecoration(
-                    color: Colors.orange,
-                    borderRadius: BorderRadius.circular(10),
+                  const SizedBox(width: 16),
+                  Text(
+                    "$counter",
+                    style: TextStyle(fontSize: 24),
                   ),
-                  child: const Center(
-                    child: Text(
-                      "+",
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
+                  SizedBox(width: 16),
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        counter++;
+                      });
+                    },
+                    child: Container(
+                      width: 40,
+                      height: 40,
+                      decoration: BoxDecoration(
+                        color: Colors.orange,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: const Center(
+                        child: Text(
+                          "+",
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
                       ),
                     ),
                   ),
-                ),
+                ],
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  final fruit = Fruit(
+                    name: 'Banana',
+                    category: 'Fruits',
+                    unitPrice: 7.0,
+                    image: 'assets/images/banana.png',
+                  );
+                  cart.addToCart(fruit);
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => CartScreen(cart: cart),
+                    ),
+                  );
+                },
+                child: Text('Add to Cart'),
               ),
             ],
-          ),
-          ElevatedButton(
-            onPressed: () {
-              final fruit = Fruit(
-                name: 'Banana',
-                category: 'Fruits',
-                unitPrice: 7.0,
-                image: 'assets/images/banana.png',
-              );
-              cart.addToCart(fruit);
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => CartScreen(cart: cart),
-                ),
-              );
-            },
-            child: Text('Add to Cart'),
-          ),
-        ],
-      ),
+          ),),)
+
+      ],)
     );
   }
 }
